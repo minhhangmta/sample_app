046119bbcd53578455df4c25ff9248739029b1d0
minhhangmta <minhhang.mta@gmail.com>
2017-04-24 13:07:37 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t ".not_found"
+      redirect_to root_path
+    end
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".signup_success"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
